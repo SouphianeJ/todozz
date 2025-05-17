ed53a78880d99bc3a2aab925a42c1a1984e5df69
Souphiane Jender <85786227+SouphianeJ@users.noreply.github.com>
2025-05-17 19:38:17 +0200

    Create firestoreService.ts

---
+++ lib/firestoreService.ts
@@ -0,0 +1,19 @@
+import { cert, getApps, initializeApp } from 'firebase-admin/app';
+import { getFirestore, FieldValue, Timestamp } from 'firebase-admin/firestore';
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT env variable is not defined.');
+}
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+if (!getApps().length) {
+  initializeApp({
+    credential: cert(serviceAccount),
+  });
+}
+
+const db = getFirestore();
+const todosCollection = db.collection('todos');
+
+export { db, todosCollection, FieldValue, Timestamp };
